b14482511af6e56077888bd7c302bfc2bad94127
ramidisanto <rdisanto@frba.utn.edu.ar>
2025-06-12 17:03:17 -0300

    Coreccion

---
+++ TP_Metamapa/ModuloProxy/src/main/java/Modelos/Entidades/Hecho.java
@@ -0,0 +1,31 @@
+package Modelos.Entidades;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class Hecho {
+
+        private String titulo;
+        private String descripcion;
+        private String contenido;
+        private String contenidoMultimedia;
+        private String fecha;
+        private String ubicacion;
+        private String origen;
+
+        public Hecho(String titulo, String descripcion, String contenido,
+                     String contenidoMultimedia, String fecha, String ubicacion) {
+            this.titulo = titulo;
+            this.descripcion = descripcion;
+            this.contenido = contenido;
+            this.contenidoMultimedia = contenidoMultimedia;
+            this.fecha = fecha;
+            this.ubicacion = ubicacion;
+            this.origen = "Demo";
+        }
+
+}
+
