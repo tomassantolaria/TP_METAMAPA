31a448f0fce95849fc3cde079ecf2384dbcc1caa
milagrosarce <miarce@frba.utn.edu.ar>
2025-09-26 10:49:37 -0300

    Co-authored-by: Mora <mocontreras@frba.utn.edu.ar>

---
+++ TP_Metamapa/ModuloEstadisticas/src/main/java/Controlador/EstadisticasControlador.java
@@ -0,0 +1,73 @@
+package Controlador;
+
+import Servicio.EstadisticasServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*; 
+import Servicio.Modelos.Exceptions.ColeccionNoEncontradaException;
+import Servicio.Modelos.Exceptions.CategoriaNoEncontradaException;
+
+
+@RestController
+@RequestMapping("/estadisticas")
+public class EstadisticasControlador{
+
+    private final EstadisticasServicio estadisticasServicio;
+
+    public EstadisticasControlador(EstadisticasServicio estadisticasServicio) {
+        this.estadisticasServicio = estadisticasServicio;
+    }
+
+    @GetMapping("/coleccion/{idColeccion}/provincia-max-hechos")
+    public ResponseEntity<String> obtenerProvinciaConMasHechos(@PathVariable Long idColeccion) {
+        try{
+            String provincia = estadisticasServico.provinciaConMasHechos(idColeccion);
+            return ResponseEntity.status(200).body(provincia);
+        }
+        catch(ColeccionNoEncontradaException e){
+            return ResponseEntity.status(404).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/coleccion/{idColeccion}/categoria-max-hechos")
+    public ResponseEntity<String> obtenerCategoriaConMasHechos(@PathVariable Long idColeccion){
+        try{
+            String categoria = estadisticasServicio.categoriaConMasHechos(idColeccion);
+            return ResponseEntity.status(200).body(categoria);
+        }catch (ColeccionNoEncontradaException e){
+            return ResponseEntity.status(404).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/hechos/{categoria}/provincia-max-hechos-categoria")
+    public ResponseEntity<String> obtenerProvinciaConMasHechosDeCategoria(@PathVariable String categoria){
+        try{
+            String provincia = estadisticasServicio.provinciaConMasHechosDeCategoria(categoria);
+            return ResponseEntity.status(200).body(provincia);
+        } catch(CategoriaNoEncontradaException e){
+            return ResponseEntity.status(404).body(e.getMessage());
+        }
+    }
+    
+    @GetMapping("/categoria/{categoria}/hora-max-hechos")
+    public ResponseEntity<Integer> obtenerHoraConMasHechos(@PathVariable String categoria){
+        try{ 
+            Integer hora = estadisticasServicio.obtenerHoraConMasHechos(categoria);
+            return ResponseEntity.status(200).body(hora);
+        }catch (CategoriaNoEncontradaException e){
+            return ResponseEntity.status(404).body("Categoría no encontrada");
+        }
+    }
+
+    @GetMapping("/solicitudes/spam/total")
+    public ResponseEntity<Longong> obtenerCantidadSolicitudesSpam(){
+        Long cantidad = estadisticasServicio.cantidadSolicitudesSpam();
+        return ResponseEntity.status(200).body(cantidad);
+    }  
+    
+    
+
+
+}
