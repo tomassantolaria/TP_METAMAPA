59676dbf0ddb0ef426ec64a6cabcf87b52c4a6b0
ramidisanto <rdisanto@frba.utn.edu.ar>
2025-10-15 14:08:48 -0300

    andas algunos colores de la pagina principal otro no!!

--- TP_Metamapa/FrontEnd/tailwind.config.js
+++ TP_Metamapa/FrontEnd/tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     './src/main/resources/**/*.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            'pastel-blue': {
+                            '50': '#f0f7ff',
+                            '100': '#e0eefd',
+                            '200': '#caddfa',
+                            '300': '#a6c6f6',
+                            '400': '#7daaf0',
+                            '500': '#5d8ce9',
+                            '600': '#4774d8',
+                            '700': '#3a5ebf',
+                            '800': '#334e9b',
+                            '900': '#2f457a',
+                            '950': '#1e2a4a',
+                          },
+        }
+    },
   },
   plugins: [],
 }
