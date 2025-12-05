6020017204d2905e1e7e90c7036bff486fc99891
moracontreras <mocontreras@frba.utn.edu.ar>
2025-12-05 20:49:39 -0300

    listo graphQL

---
+++ TP_Metamapa/ModuloAgregador/src/main/java/Configuracion/IpAccessInterceptor.java
@@ -0,0 +1,40 @@
+package Configuracion;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class IpAccessInterceptor implements HandlerInterceptor {
+
+    private static final String X_FORWARDED_FOR = "X-Forwarded-For";
+    private final IpAccessProperties properties;
+
+    public IpAccessInterceptor(IpAccessProperties properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String clientIp = request.getHeader(X_FORWARDED_FOR);
+
+        if (clientIp == null || clientIp.isEmpty()) {
+            clientIp = request.getRemoteAddr();
+        }
+
+        if (clientIp != null && clientIp.contains(",")) {
+            clientIp = clientIp.split(",")[0].trim();
+        }
+
+        if (properties.getBlacklist().contains(clientIp)) {
+            System.out.println("BLOQUEADO ESTA EN LA LISTA NEGRA: " + clientIp);
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return false;
+        }
+
+        return true;
+    }
+}
+
